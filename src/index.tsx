09444daf6d2bac5765babd5c1681d6022231ac36
Thomas Gnandt <thomas.gnandt@mayflower.de>
2022-07-22 11:41:37 +0200

    cleanup and icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { Map } from "./leaflet/Map";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
@@ -8,15 +6,8 @@ import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    // <React.StrictMode>
     <Provider store={store}>
         <CssBaseline />
         <Map />
     </Provider>
-    // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
